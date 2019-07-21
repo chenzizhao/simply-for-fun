29396ae2de1bfb2a4d3c3c7f2b86148f47d3b184
Zizhao Chen <zizhao.chen@mail.utoronto.ca>
2019-07-21 13:15:53 -0400

    comment DFS

--- mit_ocw_6.0002_A2/ps2.py
+++ mit_ocw_6.0002_A2/ps2.py
@@ -46,13 +46,17 @@ def load_map(map_filename):
     g = Digraph()
     with open(map_filename) as f:
         for line in f:
+            # parse the line
             src, dest, total_dist, outdoor_dist = line.split()
+            # create nodes
             nsrc = Node(src)
             ndest = Node(dest)
+            # add nodes if not already in the map
             if not g.has_node(nsrc): 
                 g.add_node(nsrc)
             if not g.has_node(ndest):
                 g.add_node(ndest)
+            # add edge to the map
             g.add_edge(WeightedEdge(nsrc, ndest, int(total_dist), int(outdoor_dist)))
     return g
 
@@ -109,27 +113,31 @@ def get_best_path(digraph, start, end, path, max_dist_outdoors, best_dist,
 
     Recursive helper function
     """
-    # TODO refactor the return format
+    # for simplicity
     g = digraph
+
+    # check if start and end are valid
     startn = Node(start)
     endn = Node(end)
-
     if not (g.has_node(startn) and g.has_node(endn)):
         raise ValueError
     
     # DEBUG:
     # print(f'start = {start}, end = {end}, path = {path}, max_dist_outdoors = {max_dist_outdoors}, best_dist = {best_dist}, best_path = {best_path}')
 
+    # update path, which keeps track of current path, total_dist and outdoor_dist in a list
     if path[0] != []:
         prevEdge = g.get_edge(path[0][-1], start)
         path[1] += prevEdge.get_total_distance()
         path[2] += prevEdge.get_outdoor_distance()
         path[0] += [start]
-    else: 
+    else: # it is the first recursion, where there is not previous node nor distance travelled
         path[0] = [start]
+    
     # DEBUG:
     # print(f'current path = {path}')
     
+    # base case
     if path[2] > max_dist_outdoors:
         # DEBUG
         # print("Exceeding max_dist_outdoors already!")
@@ -139,19 +147,25 @@ def get_best_path(digraph, start, end, path, max_dist_outdoors, best_dist,
         # print(f'find a path! path = {path[0:2]}')
         return path[0: 2]
 
+    # recursive case
     startn = g.get_node(start)
-
     for edge in g.get_edges_for_node(startn):
         childn = edge.get_destination()
         child = childn.get_name()
-        while path[0][-1] != start:
+        while path[0][-1] != start: # make sure the last item of current path is "start"
             path[0].pop()
-        if child not in path[0]:
-            if best_path == None or path[1] < best_dist:
+        if child not in path[0]:    # avoid loops
+            if best_path == None or path[1] < best_dist:    #the new path may be a best path
+                # solve the problem recursively: find the best path from child to end
+                # TODO: explain why "path.copy()" works but "path" does not? 
                 newPath = get_best_path(g, child, end, path.copy(), max_dist_outdoors, best_dist, best_path)
-                if newPath[0] != None and newPath[1] < best_dist:
+                if newPath[0] != None and newPath[1] < best_dist:   #final check if the return is indeed a better path than the current best_path
+                    # TODO: explain why "path.copy()" works but "path" does not? 
+                    # ANS: maybe due to the fact that asssignment only redirects the reference while copying actually memorizes the values
                     best_path = newPath[0].copy()
                     best_dist = newPath[1]
+    # after iterating over all childs, return best path and best_dist (might have been updated, might have not)
+    # TODO: consider return a tuple instead of a list -- this may solve the previous reference problem
     return [best_path, best_dist]
 
 # Problem 3c: Implement directed_dfs
@@ -185,11 +199,15 @@ def directed_dfs(digraph, start, end, max_total_dist, max_dist_outdoors):
 
     Wrapper function
     """
+    # Sanity check: maybe extra
     if start == end:
         return [start]
+    # set up the recurison
     path = get_best_path(digraph, start, end, [[], 0, 0], max_dist_outdoors, float('inf'), None)
     if path[1] <= max_total_dist:
+        # valid path found
         return path[0]
+    # valid path not found: path[1] == inf or path[1] > mac_total_dist
     raise ValueError
 
 # ================================================================
