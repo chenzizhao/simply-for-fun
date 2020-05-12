df70b823fe62194ebc728723bc26e3df57623640
chenzizhao <zizhao.chen@mail.utoronto.ca>
2020-05-12 00:09:53 -0400

    set up express static

--- Ex_Files_Building_Website_NodeJS_Express/server.js
+++ Ex_Files_Building_Website_NodeJS_Express/server.js
@@ -1,13 +1,21 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 
 const app = express();
 
 // Usually 8080 for web servers, 3000 for express servers
 const port = 3000;
 
-// Get route
+// Middle ware
+app.use(express.static(path.join(__dirname, './static')));
+
+// Get route, send html files
 app.get('/', (request, response)=>{
-    response.send('Hello Express');
+    response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response)=>{
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, ()=>{
