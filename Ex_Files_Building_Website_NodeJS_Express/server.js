00a827dcd7a386cf56f68a61e3d8c3e1d2422121
chenzizhao <zizhao.chen@mail.utoronto.ca>
2020-05-12 09:53:01 -0400

    render page title with ejs

--- Ex_Files_Building_Website_NodeJS_Express/server.js
+++ Ex_Files_Building_Website_NodeJS_Express/server.js
@@ -6,12 +6,18 @@ const app = express();
 // Usually 8080 for web servers, 3000 for express servers
 const port = 3000;
 
+// Set EJS as the template engine
+// (no need to require EJS as express will discover it)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 // Middle ware
 app.use(express.static(path.join(__dirname, './static')));
 
 // Get route, send html files
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, './static/index.html'));
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.get('/speakers', (request, response) => {
