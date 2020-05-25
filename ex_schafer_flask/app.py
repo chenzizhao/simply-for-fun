67e9ff8a2c7d81b8ff4ec9005ebb7886bfc11212
chenzizhao <zizhao.chen@mail.utoronto.ca>
2020-05-24 20:17:13 -0400

    Adding custom info

--- ex_schafer_flask/app.py
+++ ex_schafer_flask/app.py
@@ -2,6 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+experiences = [
+    {
+        'company':'SOCAAR, University of Toronto',
+        'role':'Research Assistant',
+        'time':'March 2019 - August 2019',
+    },
+
+    {
+        'company':'Faculty of Law, University of Toronto',
+        'role':'IT Help Desk Analyst',
+        'time':'October 2019 - April 2020',
+    },
+
+    {
+        'company':'Google',
+        'role':'STEP Intern',
+        'time':'May 2020 - August 2020',
+    }
+]
+
+
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -9,7 +32,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", experiences=experiences)
 
 if __name__ == "__main__":
     app.run(debug=True)
